8d09447feba956f21febefb7132f558f804e7ca4
hastagaming <rip.hastagaming@gmail.com>
2026-05-28 08:10:52 +0700

    fix the same thing

--- lumina-api/build.gradle.kts
+++ lumina-api/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     api(project(":lumina-common"))
     api(project(":lumina-event"))
     api(project(":lumina-scheduler"))
+    api(project(":lumina-command"))
     api(project(":lumina-world"))
     api(project(":lumina-entity"))
     api(project(":lumina-logging"))
